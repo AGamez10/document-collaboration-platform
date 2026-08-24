6e157884f0d251f9824d1bdf23f0d3753ee9b2fa
AGamez10 <00andresgamez@gmail.com>
2026-08-24 12:08:16 -0500

    test: add application context smoke test

---
+++ src/test/java/office_platform/office_platform/OfficePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package office_platform.office_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
